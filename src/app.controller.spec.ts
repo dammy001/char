dc9b2813e0a5fad81773631f0c35a063984072c5
Damilare Anjorin <damilareanjorin1@gmail.com>
2021-10-06 18:44:43 +0100

    fix test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "welcome to char!"', () => {
       expect(appController.welcome()).toBe('welcome to char');
     });
   });
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthModule } from './api/auth/auth.module';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { AppController } from './app.controller';
 
 @Module({
+  controllers: [AppController],
   providers: [AppService],
   imports: [AuthModule],
 })
