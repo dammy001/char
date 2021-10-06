c1ac5c0001b344969c5625f5cbe1313571df7608
Damilare Anjorin <damilareanjorin1@gmail.com>
2021-10-06 18:31:12 +0100

    update

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.welcome()).toBe('welcome to reluctive');
+      expect(appController.welcome()).toBe('welcome to char');
     });
   });
 });
